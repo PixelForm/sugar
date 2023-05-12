b564ca76f79255f3275fcfc7b7fe0023b3ed92cd
PixelForm <leanderdemooij2000@gmail.com>
2023-05-12 12:12:49 +0200

    modified files

--- packages/readtime/src/main.ts
+++ packages/readtime/src/main.ts
@@ -6,9 +6,10 @@ let index = 0
 
 for( ; index < textualElements.length; index++ ) {
     const text = textualElements[index].innerHTML
-    const duration = readtime( text )
+    const duration: number = Math.round( readtime( text ) )
+    const readTime: string = duration < 1 ? '1' : `${ duration }`
 
     const closest = textualElements[index].parentElement?.querySelector('[est-readtime]')
 
-    if( closest ) closest.textContent = duration < 1 ? '1' : `${Math.round( duration )}`
+    if( closest ) closest.textContent = readTime
 }
